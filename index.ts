86e8ebc3be80b05a58e797f28abf09ac7845287d
Anwesh43 <anweshthecool0@gmail.com>
2020-04-02 04:31:30 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -8,6 +8,17 @@ const sizeFactor : number = 2.9
 const nodes : number = 5
 const parts : number = 5
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n :number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
 class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
